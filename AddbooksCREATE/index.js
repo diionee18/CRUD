919874d321e964c4b959fbc5fb65837a53cfced2
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-09-25 22:33:19 +0200

    laddar upp filer

---
+++ AddbooksCREATE/index.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+const dynamoClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-north-1' });
+
+exports.handler = async (event) => {
+   
+    const { BookName, Author } = event.body;
+
+    const params = {
+        TableName: 'Books',
+        Item: {
+            "BookId": AWS.util.uuid.v4(),
+            "BookName": BookName, 
+            "Author": Author 
+        }
+    };
+
+    try {
+        await dynamoClient.put(params).promise();
+        return {
+            statusCode: 201,
+            body: ''
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify('Error creating book: ' + error.message)
+        };
+    }
+};
+
